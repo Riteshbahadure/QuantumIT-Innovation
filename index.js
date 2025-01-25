60717ec96ac0bae798331057aed6e60a420c26e2
Rutuja Bahadure <bahadurerutu558@gamil.com>
2025-01-25 15:11:42 +0530

    updated

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require("dotenv").config()
 
 mongoose.connect(process.env.MONGO_URL)
 const app = express()
+app.use(express.static("dist"))
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
@@ -16,7 +18,7 @@ app.use(cors({
 // app.use("/api/admin", require("./route/admin.route"))
 app.use("/api/user", require("./route/user.route"))
 app.use("*", async (req, res) => {
-    res.status(404).json({ message: "Resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 app.use((error, req, res, next) => {
     res.status(500).json({ message: error.message || "something wen wrong" })
